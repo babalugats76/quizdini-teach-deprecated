8aa10113505ee934dbe42c6dec657037db0faf38
James Colestock <james@colestock.com>
2019-01-31 21:53:59 -0700

    Added eslint line disable

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import MatchGameCreator from './MatchGameCreator';
 import './App.css';

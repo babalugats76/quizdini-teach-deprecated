fc5092aa5c987912f23a22cfbb21d110daede464
James Colestock <james@colestock.com>
2019-02-01 14:58:33 -0700

    Add Formik to project, etc.

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ This project is the teacher front-end for Quizdini 2.0
 This project uses the React Router module, installed via `npm install react-router-dom` 
 
 Documentation can be found at [React Router: Declarative Routing for React.js](https://reacttraining.com/react-router/)
+
+## Formik
+
+I am going to try to use Formik in this project to simplify the gnarly aspects of creating dynamic forms in React
+
+To install: `npm i formik`
+
+Documentation can be found [here](https://jaredpalmer.com/formik/docs/overview)
--- src/MatchGameCreator.js
+++ src/MatchGameCreator.js
@@ -1,87 +1,115 @@
 import React, { Component } from 'react';
+import { Formik, Field } from 'formik';
 
 class MatchGameCreator extends Component {
-  
-    constructor(props) {
-      super(props);
-      this.state = {
-        placeholder: "term, definition",
-        text: '',
-        json: '',
-        terms: [],
-        definitions: []
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      placeholder: "term, definition",
+      text: '',
+      json: '',
+      terms: [],
+      definitions: []
+    }
+  }
+
+  componentDidMount() {
+    //setTimeout(() => { this.setState({ text: '', json: ''}); }, 1000);
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  getItems(str) {
+
+    let json = str.split('\n');
+    let items = new Array(0).fill(null);
+
+    json.reduce((pairs, line) => {
+      let vals = line.split(',') || [];
+      if (!Array.isArray(vals)
+        || !vals.length
+        || vals.length < 2
+        || vals[0].trim().length === 0
+        || vals[1].trim().length === 0) {
+        return items;
       }
-    }
-    
-    componentDidMount() {
-      //setTimeout(() => { this.setState({ text: '', json: ''}); }, 1000);
-    }
-    
-    componentWillUnmount() {
-      
-    }
-    
-    getItems(str) {
-      
-      let json = str.split('\n');
-      let items = new Array(0).fill(null);
-              
-      json.reduce((pairs, line) => {
-        let vals = line.split(',') || [];
-        if (!Array.isArray(vals) 
-            || !vals.length 
-            || vals.length < 2
-            || vals[0].trim().length === 0
-            || vals[1].trim().length === 0) { 
-            return items;
-        }
-        return items.push({"term": vals[0].trim(), "definition": vals[1].trim() });
-      }, items);
-      
-      return items;
-    }
-    
-    toJson(arr, indentSpaces) {
-      return ((arr && arr.length > 0) ? JSON.stringify(arr.reverse(), null, indentSpaces) : '');
-    }
-    
-    handleEditorChange(event) {     
-       
-       const text = event.target.value;
-       let items = this.getItems(text);
-       const json = this.toJson(items, 3);
-       const definitions = items.map((item) => { return item.definition; });
-       const terms = items.map((item) => { return item.term; });
-         
-       this.setState({
-         text: text,
-         json: json,
-         terms: terms,
-         definitions: definitions
-       });
-      
-    }
-    
-    render() {
-      return(<div>
-               <div className="container align-top">
-                  <Editor className="" placeholder={this.state.placeholder} text={this.state.text} onChange={(e) => this.handleEditorChange(e)}/>
-                  <JsonEditor className="" placeholder={this.toJson(this.getItems(this.state.placeholder),3)} text={this.state.json} />
-               </div>
-             </div>);
-    }
+      return items.push({ "term": vals[0].trim(), "definition": vals[1].trim() });
+    }, items);
+
+    return items;
   }
-  
-  class Editor extends React.Component {
-    render() {
-      return(<textarea className="editor w-50 align-top" placeholder={this.props.placeholder} value={this.props.text} onChange={this.props.onChange}></textarea>);
-    }
+
+  toJson(arr, indentSpaces) {
+    return ((arr && arr.length > 0) ? JSON.stringify(arr.reverse(), null, indentSpaces) : '');
   }
-  
-  class JsonEditor extends React.Component {
-    render () {
-      return (<textarea className="editor w-50 align-top" placeholder={this.props.placeholder} value={this.props.text} readOnly></textarea>);
-    }
+
+  handleEditorChange(event) {
+
+    const text = event.target.value;
+    let items = this.getItems(text);
+    const json = this.toJson(items, 3);
+    const definitions = items.map((item) => { return item.definition; });
+    const terms = items.map((item) => { return item.term; });
+
+    this.setState({
+      text: text,
+      json: json,
+      terms: terms,
+      definitions: definitions
+    });
+
   }
-  
+
+  /*render() {
+    return (<div>
+      <div className="container align-top">
+        <Editor className="" placeholder={this.state.placeholder} text={this.state.text} onChange={(e) => this.handleEditorChange(e)} />
+        <JsonEditor className="" placeholder={this.toJson(this.getItems(this.state.placeholder), 3)} text={this.state.json} />
+      </div>
+    </div>);
+  }*/
+
+  render() {
+
+    const initialValues = {
+      title: 'Short, meaningful name',
+      instructions: 'Default instructions string'
+    }
+
+    return (
+      <div class="container">
+        <h1>Match Game</h1>
+        <Formik initialValues={initialValues}>
+          {() => <div>
+            <div className="row">
+              <Field name="title" type="text" />
+            </div>
+            <div className="row">
+              <Field name="instructions" type="text" />
+            </div>
+          </div>}
+        </Formik>
+      </div>
+    );
+  }
+
+}
+
+// eslint-disable-next-line
+class Editor extends React.Component {
+  render() {
+    return (<textarea className="editor w-50 align-top" placeholder={this.props.placeholder} value={this.props.text} onChange={this.props.onChange}></textarea>);
+  }
+}
+
+// eslint-disable-next-line
+class JsonEditor extends React.Component {
+  render() {
+    return (<textarea className="editor w-50 align-top" placeholder={this.props.placeholder} value={this.props.text} readOnly></textarea>);
+  }
+}
+
 export default MatchGameCreator;

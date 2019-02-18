757babbaeb587943e6cd3e55f642c6e42ff50afd
James Colestock <james@colestock.com>
2019-02-17 17:03:22 -0700

    Started creating the layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // eslint-disable-next-line
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import Hello from './Hello';
 import MatchGame from './MatchGame';

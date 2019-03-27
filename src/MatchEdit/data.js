c0499cdf05f0238724e79e4e3c8416bf8b9e26ee
James Colestock <james@colestock.com>
2019-03-26 19:27:39 -0600

    renamed loadData

---
+++ src/MatchEdit/data.js
@@ -0,0 +1,59 @@
+import API from './api'; // Base axios instance for server interactions
+
+/**
+ * Function that uses base axios instance to fetch match data.
+ *
+ * This is function should be either wrapped in a Promise
+ * or called using alternative async/await syntax.
+ *
+ * Although axios is a Promise-based API, we need are
+ * wrapping in a Promise so that the caller can provide
+ * resolution and rejection functions; this is, essentially,
+ * the only way to "bubble up" errors.
+ *
+ * @param {string} query Customizable portion of the URI.
+ * @param {number} sleep Milliseconds to pause (used to test latency / loading).
+ * @callback {function} success Success callback (Promise resolution).
+ * @callback {function} failure Failure callback (Promise rejection).
+ */
+const loadMatch = (query, sleep, success, failure) => {
+  return new Promise((resolve, failed) => {
+    const encodedQuery = encodeURIComponent(query);
+    API.get(`match/${encodedQuery}`)
+      .then(res => {
+        return setTimeout(() => success(res.data), sleep);
+      })
+      .catch(error => {
+        let msg;
+        if (!error.response || error.code === 'ECONNABORTED') {
+          // Traps both timeouts and CORS, etc.
+          msg = 'There was a network error...';
+        } else {
+          msg = error.response.status + ' ' + error.response.statusText;
+        }
+        return failure(msg);
+      });
+  });
+};
+
+const saveMatch = (values, setSubmitting, sleep, success, failure) => {
+  return new Promise((resolve, failed) => {
+    const encodedQuery = encodeURIComponent(values.id || '');
+    API.post(`match/${encodedQuery}`, values)
+      .then(res => {
+        return setTimeout(() => success(res.data, setSubmitting), sleep);
+      })
+      .catch(error => {
+        let msg;
+        if (!error.response || error.code === 'ECONNABORTED') {
+          // Traps both timeouts and CORS, etc.
+          msg = 'There was a network error...';
+        } else {
+          msg = error.response.status + ' ' + error.response.statusText;
+        }
+        return failure(msg);
+      });
+  });
+}
+
+export { loadMatch, saveMatch };
